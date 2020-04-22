2d19aa34895384dccd51999297d6c0879385f927
alperen2 <aalperendurmuss@gmail.com>
2020-04-22 13:21:46 +0300

    bir akç düzenleme yapıldı

--- static/script.js
+++ static/script.js
@@ -1,58 +1,27 @@
 $(document).ready(function (e) {
-  $("#formUpload").on('submit', function (e) {
-    e.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: '/upload',
-      data: new FormData(this),
-      dataType: 'json',
-      contentType: false,
-      cache: false,
-      processData: false,
-      beforeSend: function () {
-        $('.submitBtn').attr("disabled", "disabled");
-        $('#fupForm').css("opacity", ".5");
-      },
-      success: function (response) {
-        console.log(response);
-
-        $(".card-text").html(response.text)
-        $(".img").attr('src', response.img)
-        $('#formUpload').css("opacity", "");
-        $("#btnUpload").removeAttr("disabled");
-      }
-    });
-  });
-
   $("#file").on('change', (e) => {
     document.getElementById('image').src = "";
-    let preview = document.getElementById('preview');
-    file = document.getElementById('file').files[0]
+    file = e.target.files[0]
     let reader = new FileReader();
 
     reader.addEventListener("load", () => {
       document.getElementById('image').src = reader.result
-    }, false)
+    }, true)
 
     if (file) { reader.readAsDataURL(file); }
   })
 
 
-  let x;
-  let y;
-  let width;
-  let height;
-  let rotate;
-  let scaleX;
-  let scaleY;
-  let cropper;
-  let image;
-
+  let x, y, width, height, rotate, scaleX, scaleY, cropper, image;
   $("#btnCrop").on('click', () => {
+    $("#file").prop('disabled', 'true')
+    $('#btnCrop').prop('disabled', 'true')
+    $('#btnCropOk').removeAttr('disabled')
+    $('#btnCropReset').removeAttr('disabled')
     image = document.getElementById('image');
     cropper = new Cropper(image,
       {
-        viewMode: 1,
+        viewMode: 0,
         crop(event) {
           x = event.detail.x
           y = event.detail.y
@@ -66,47 +35,34 @@ $(document).ready(function (e) {
 
   })
 
-  // $('#btnCropOk').on('click', () => {
-  //   var croppedimage = cropper.getCroppedCanvas().toDataURL("image/png");
-  //   $('#has').html(cropper.getCroppedCanvas());
-  //   $('#preview').src = croppedimage
-  // })
-
   $('#btnCropOk').on('click', () => {
-    let croppedimage = cropper.getCroppedCanvas(
-      {
-        width: 160,
-        height: 90,
-        minWidth: 256,
-        minHeight: 256,
-        maxWidth: 4096,
-        maxHeight: 4096,
-        fillColor: '#fff',
-        imageSmoothingEnabled: false,
-        imageSmoothingQuality: 'high',
-      }).toBlob(blob => {
-        const formData = new FormData();
-        console.log(blob);
-        
-        formData.append('file', blob, `${Math.round(Math.random()*100000)}.png`);
-        $.ajax('/upload', {
-          method: 'POST',
-          data: formData,
-          processData: false,
-          contentType: false,
-          success() {
-            console.log('Upload success');
-          },
-          error() {
-            console.log('Upload error');
-          },
-        });
+    let croppedimage = cropper.getCroppedCanvas().toBlob(blob => {
+      const formData = new FormData();
 
-      }, 'image/jpeg', 0.95)
+      formData.append('file', blob, `${Math.round(Math.random() * 100000)}.png`);
+      $.ajax('/upload', {
+        method: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success(response) {
+          console.log(response)
+          data = JSON.parse(response)
+          $(".card-text").html(data.text)
+          $(".img").attr('src', data.img)
+        },
+        error(e) {
+          console.log(e);
+        },
+      });
 
-
-
-
+    }, 'image/jpeg', 0.95)
+  })
+  $('#btnCropReset').click(() => {
+    $("#file").removeAttr("disabled");
+    $('#btnCrop').removeAttr("disabled");
+    $('#btnCropOk').prop("disabled", 'true');
+    cropper.destroy()
   })
 });
 
